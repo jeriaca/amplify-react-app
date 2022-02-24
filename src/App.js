e4a3e47876c55de1ac14cc084fad21721ebb2de5
Jeri <jeriacasper@gmail.com>
2022-02-23 18:27:06 -0600

    Got it working

--- src/App.js
+++ src/App.js
@@ -7,13 +7,26 @@ import { API } from 'aws-amplify';
 import './App.css';
 
 const App = () => {
+  
+  // Create additional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 })
+
+  // Create a new function to allow users to update the input values
+  const updateInputValues = (type, value) => {
+    updateInput({ 
+      ...input
+      , [type]: value 
+    });
+  };
+
   // Create coins variable and set to empty array
   const [coins, updateCoins] = useState([]);
 
   // Define function to all API
-  const fetchCoins = async () => {
-    const data = await API.get('cryptoapi', '/coins');
-    console.log(data);
+    const fetchCoins = async () => {
+    const { limit, start } = input;
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
+    
     updateCoins(data.coins);
   };
 
@@ -27,6 +40,22 @@ const App = () => {
 
   return (
     <div className="App">
+
+      <input
+        onChange={e => updateInputValues('limit', e.target.value)}
+        placeholder="Enter a limit"
+      />
+
+      <input
+        placeholder="Enter a starting value"
+        onChange={e => updateInputValues('start', e.target.value)}
+      />
+
+      <button 
+        onClick={fetchCoins}
+      >
+        Fetch Coins
+      </button>
       {
         coins.map((coin) => (
           <div key={coin.name}>
